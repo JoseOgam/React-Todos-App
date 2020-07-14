649664ce8cb95ab91d1f8dc00bad1259a735fdce
JoseOgam <jotienoochieng@gmail.com>
2020-07-14 14:42:13 -0400

    styling with superpower css(sass)

--- src/component/actions.js
+++ src/component/actions.js
@@ -3,7 +3,9 @@ import React from "react";
 const Actions = (props) => {
   return (
     <div>
-      <button onClick={props.handlepick}>Show what todo</button>
+      <button className="big-button" onClick={props.handlepick}>
+        Show what todo
+      </button>
     </div>
   );
 };
--- src/component/header.js
+++ src/component/header.js
@@ -3,8 +3,10 @@ import React from "react";
 const Header = (props) => {
   return (
     <div className="header">
-      <h1 className="header-title">{props.title}</h1>
-      <h2 className="header-subtitle">{props.subtitle}</h2>
+      <div className="container">
+        <h1 className="header-title">{props.title}</h1>
+        <h2 className="header-subtitle">{props.subtitle}</h2>
+      </div>
     </div>
   );
 };
