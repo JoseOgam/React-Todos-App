5e491f7fc224c81d7d4eb92d7dc8e8a19874ec11
JoseOgam <jotienoochieng@gmail.com>
2020-07-14 14:09:54 -0400

    styling with superpower css(sass)

--- src/component/header.js
+++ src/component/header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = (props) => {
   return (
-    <div>
-      <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+    <div className="header">
+      <h1 className="header-title">{props.title}</h1>
+      <h2 className="header-subtitle">{props.subtitle}</h2>
     </div>
   );
 };
